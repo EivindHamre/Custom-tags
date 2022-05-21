8473d871fe29e804e0d7505a13fcac48cfedf20f
Eivind Hamre <eivindhamre04@gmail.com>
2022-05-21 15:44:05 +0200

    test

--- range.js
+++ range.js
@@ -1,7 +1,7 @@
 const doubleRangeProto = Object.create(HTMLElement.prototype);
 
 doubleRangeProto.createdCallback = function () {
-    this.innerHTML = "<input type='range' min='0' max='100' value='50' step='1'><br>";
+    this.innerHTML = "<b>Double Range</b>";
 }
 
 const doubleRange = document.registerElement("double-range", {
